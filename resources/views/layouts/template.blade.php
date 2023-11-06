bbe66df618f758fba0cecf40b235f6acd373cd50
alirfanyasin <risayunita21@gmail.com>
2023-11-06 14:20:57 +0700

    fix: responsive

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title')</title>
-  <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/main-logo.ico') }}">
+  <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/main-logo.png') }}">
   {{-- BOOTSTRAP 5.3.2 CSS --}}
   {{-- <link rel="stylesheet" href="{{ url('node_modules/bootstrap/dist/css/bootstrap.min.css') }}"> --}}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
