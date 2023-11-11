3ef6231e7c1da51d88a4444ce396460dbabcf1ee
alirfanyasin <risayunita21@gmail.com>
2023-11-12 01:28:40 +0700

    feat: new detail UI

--- resources/views/pages/app/news.blade.php
+++ resources/views/pages/app/news.blade.php
@@ -12,124 +12,106 @@
     </div>
     <div class="row">
       <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
+        <a href="{{ route('admin.news.show') }}" class="text-decoration-none">
+          <div class="p-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+            <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
+            <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+            <div class="footer d-flex justify-content-between align-items-center">
+              <div>
+                <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
+              </div>
+              <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
+                <small>3 hour ago</small> &nbsp;
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       </div>
       <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
+        <a href="{{ route('admin.news.show') }}" class="text-decoration-none">
+          <div class="p-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+            <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
+            <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+            <div class="footer d-flex justify-content-between align-items-center">
+              <div>
+                <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
+              </div>
+              <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
+                <small>3 hour ago</small> &nbsp;
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       </div>
       <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
+        <a href="{{ route('admin.news.show') }}" class="text-decoration-none">
+          <div class="p-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+            <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
+            <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+            <div class="footer d-flex justify-content-between align-items-center">
+              <div>
+                <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
+              </div>
+              <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
+                <small>3 hour ago</small> &nbsp;
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       </div>
       <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
+        <a href="{{ route('admin.news.show') }}" class="text-decoration-none">
+          <div class="p-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+            <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
+            <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+            <div class="footer d-flex justify-content-between align-items-center">
+              <div>
+                <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
+              </div>
+              <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
+                <small>3 hour ago</small> &nbsp;
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       </div>
       <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
+        <a href="{{ route('admin.news.show') }}" class="text-decoration-none">
+          <div class="p-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+            <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
+            <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+            <div class="footer d-flex justify-content-between align-items-center">
+              <div>
+                <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
+              </div>
+              <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
+                <small>3 hour ago</small> &nbsp;
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       </div>
       <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
+        <a href="{{ route('admin.news.show') }}" class="text-decoration-none">
+          <div class="p-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+            <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
+            <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+            <div class="footer d-flex justify-content-between align-items-center">
+              <div>
+                <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
+              </div>
+              <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
+                <small>3 hour ago</small> &nbsp;
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-3">
-        <div class="p-3 text-white"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <img src="{{ asset('assets/img/img-1.png') }}" alt="" width="100%">
-          <p class="text-left mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-          <div class="footer d-flex justify-content-between align-items-center">
-            <div>
-              <small class="fw-light" style="color: rgba(255, 255, 255, 0.573);">180 Views</small>
-            </div>
-            <div class="fw-light" style="color: rgba(255, 255, 255, 0.573);">
-              <small>3 hour ago</small> &nbsp;
-            </div>
-          </div>
-        </div>
+        </a>
       </div>
     </div>
   </section>
