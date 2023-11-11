fb79277609ab7d3cb920063261f0159c76e5c0aa
alirfanyasin <risayunita21@gmail.com>
2023-11-12 02:20:01 +0700

    fix: dashboard

--- resources/views/pages/app/dashboard.blade.php
+++ resources/views/pages/app/dashboard.blade.php
@@ -53,21 +53,9 @@
             </div>
             <div class="text-white p-3 mb-3"
               style="background: rgba(255, 255, 255, 0.05); border-radius: 10px; backdrop-filter: blur(5px);">
-              <div class="fw-semibold">Mobile Development</div>
-              <div class="fs-1 fw-bold">15:30</div>
-              <div class="fw-light">13 November 2023</div>
-            </div>
-            <div class="text-white p-3 mb-3"
-              style="background: rgba(255, 255, 255, 0.05); border-radius: 10px; backdrop-filter: blur(5px);">
               <div class="fw-semibold">Comp. Programming</div>
               <div class="fs-1 fw-bold">07:00</div>
               <div class="fw-light">14 November 2023</div>
-            </div>
-            <div class="text-white p-3 mb-3"
-              style="background: rgba(255, 255, 255, 0.05); border-radius: 10px; backdrop-filter: blur(5px);">
-              <div class="fw-semibold">Web Development</div>
-              <div class="fs-1 fw-bold">13:00</div>
-              <div class="fw-light">13 November 2023</div>
             </div>
           </div>
         </div>
