183aab7e264f3d754e9434266d1a18ea7e0d668c
alirfanyasin <risayunita21@gmail.com>
2023-11-12 02:12:16 +0700

    fix: chart

--- app/Charts/MonthlyContentChart.php
+++ app/Charts/MonthlyContentChart.php
@@ -16,8 +16,8 @@ class MonthlyContentChart
     public function build(): \ArielMejiaDev\LarapexCharts\AreaChart
     {
         return $this->chart->areaChart()
-            ->addData('Physical sales', [40, 93, 35, 42, 18, 82, 45, 78, 34, 89])
-            ->addData('Digital sales', [70, 29, 77, 28, 55, 45, 56, 67, 90, 46])
+            ->addData('News', [40, 93, 35, 42, 18, 82, 45, 78, 34, 89])
+            ->addData('Users', [70, 29, 77, 28, 55, 45, 56, 67, 90, 46])
             ->setHeight(400)
             ->setColors(['#01012D', '#FFFFFF'])
             ->setXAxis(['Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'])
