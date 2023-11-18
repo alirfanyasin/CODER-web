b679af7f502832a9202b6522a3a1fde7ebb96f53
alirfanyasin <risayunita21@gmail.com>
2023-11-18 19:48:00 +0700

    feat: upload multiple with drag-and-drop or browse

--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -3,17 +3,90 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Gallery;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
 
 class GalleryController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         return view('pages.app.gallery');
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('pages.app.gallery_create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validate the file uploads
+        $request->validate([
+            'img.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust based on your requirements
+        ]);
+        // Check if files were uploaded
+        if ($request->hasFile('img')) {
+            foreach ($request->file('img') as $file) {
+                $rdmStr = Str::random(5);
+                $fileName = $rdmStr . '_' . $file->getClientOriginalName();
+                $file->storeAs('public/gallery', $fileName);
+
+                // Create a new Gallery model instance
+                Gallery::create([
+                    'img' => $fileName
+                ]);
+            }
+
+            // Return a success response
+            return response()->json(['message' => 'Files uploaded successfully'], 200);
+        }
+
+        // Handle the case when no files are uploaded
+        return response()->json(['error' => 'No files uploaded'], 400);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- resources/views/pages/app/gallery_create.blade.php
+++ resources/views/pages/app/gallery_create.blade.php
@@ -32,28 +32,24 @@
 
   <div class="row">
     <div class="col">
-      <div id="drop-area" ondrop="dropHandler(event)" ondragover="dragOverHandler(event)">
-        <label for="file-input" style="cursor: pointer;">
-          <iconify-icon icon="gridicons:image-multiple" width="100px" class="text-white"></iconify-icon>
-        </label>
-        <p class="text-white"><label for="file-input" style="cursor: pointer;">Drag and drop files here or click to
-            select</label> </p>
-        <input type="file" id="file-input" multiple onchange="handleFileSelect(event)">
-      </div>
-
-      <div class="row">
-        <div class="col-4">
-          <!-- Use class for multiple images -->
-          <img src="" alt="" class="w-100 img-result" width="100%">
+      <form action="{{ route('admin.gallery.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div id="drop-area" ondrop="dropHandler(event)" ondragover="dragOverHandler(event)">
+          <label for="file-input" style="cursor: pointer;">
+            <iconify-icon icon="gridicons:image-multiple" width="100px" class="text-white"></iconify-icon>
+          </label>
+          <p class="text-white"><label for="file-input" style="cursor: pointer;">Drag and drop files here or click to
+              select</label> </p>
+          {{-- <input type="file" id="file-input" name="img[]" multiple> --}}
+          <input type="file" id="file-input" name="img[]" multiple onchange="handleFileSelect(event)">
         </div>
-        <div class="col-md-8">
-
-        </div>
-      </div>
-
+        <button type="submit" class="btn-main mt-3" style="border: none;">Submit</button>
+      </form>
       <div id="file-list"></div>
     </div>
   </div>
+
+
   <script>
     function dragOverHandler(event) {
       event.preventDefault();
@@ -79,30 +75,48 @@
 
       for (const file of files) {
         const fileSizeKB = (file.size / 1024).toFixed(2);
+
+        // Create a new file item div
         const fileItem = document.createElement('div');
+        fileItem.classList.add('file-item', 'mb-3');
+
+        // Create a new progress bar for each file
+
 
         // Create a new row element for each file
         const divRow = document.createElement('div');
         divRow.classList.add('row');
+        divRow.setAttribute('style', 'background: rgba(255, 255, 255, 0.13)');
+        divRow.classList.add('py-3', 'rounded-3');
+
 
         // Create a new column element for the image
         const divCol1 = document.createElement('div');
-        divCol1.classList.add('col-md-3', 'mb-3');
+        divCol1.classList.add('col-md-3');
 
         // Create a new column element for file information
         const divCol2 = document.createElement('div');
-        divCol2.classList.add('col-md-9', 'mb-3');
+        divCol2.classList.add('col-md-9');
 
         // Populate divCol2 with file information
         divCol2.innerHTML = `<p class="text-white">File Name: ${file.name}</p>
-                        <p class="text-white">File Size: ${fileSizeKB} KB</p>
-                        <p class="text-white">File Type: ${file.type}</p>`;
+                              <p class="text-white">File Size: ${fileSizeKB} KB</p>
+                              <p class="text-white">File Type: ${file.type}</p>`;
+
+        const progressBar = document.createElement('div');
+        progressBar.classList.add('progress', 'mb-2');
+        progressBar.innerHTML =
+          '<div class="progress-bar bg-danger" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>';
+        fileItem.appendChild(progressBar);
+
+        divCol2.appendChild(progressBar);
 
         // Create a new image element for each file
         const image = document.createElement('img');
         image.src = URL.createObjectURL(file);
         image.alt = 'File Preview';
         image.classList.add('w-100', 'img-result');
+        image.style.height = '250px';
 
         // Append image to divCol1
         divCol1.appendChild(image);
@@ -111,10 +125,44 @@
         divRow.appendChild(divCol1);
         divRow.appendChild(divCol2);
 
-        // Append the row to the file item, and file item to the file list
+        // Append the row to the file item
         fileItem.appendChild(divRow);
+
+        // Append the file item to the file list
         fileList.appendChild(fileItem);
+
+        // Upload file and update progress bar
+        uploadFile(file, progressBar);
       }
+    }
+
+    function uploadFile(file, progressBar) {
+      const xhr = new XMLHttpRequest();
+      const formData = new FormData();
+      formData.append('img[]', file);
+
+      xhr.open('POST', '/admin/gallery/store', true);
+
+      xhr.upload.onprogress = function(e) {
+        if (e.lengthComputable) {
+          const percentage = (e.loaded / e.total) * 100;
+          progressBar.querySelector('.progress-bar').style.width = percentage + '%';
+          progressBar.querySelector('.progress-bar').innerHTML = percentage.toFixed(0) + '%';
+        }
+      };
+
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          // Handle successful upload
+          const response = JSON.parse(xhr.responseText);
+          console.log(response.message); // Log the success message
+        } else {
+          // Handle upload failure
+          console.error('File upload failed');
+        }
+      };
+
+      xhr.send(formData);
     }
   </script>
 @endsection
