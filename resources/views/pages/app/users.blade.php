aa19db7a7f50ed1c6539087b5fa921bfe2d08cb7
alirfanyasin <risayunita21@gmail.com>
2023-11-20 14:59:02 +0700

    feat: read data user

--- resources/views/pages/app/users.blade.php
+++ resources/views/pages/app/users.blade.php
@@ -9,186 +9,28 @@
     </div>
 
     <div class="row">
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
+      @foreach ($data as $user)
+        <div class="col-md-3 mb-3">
+          <div class="p-4 text-white text-center"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+            <div class="d-flex justify-content-center">
+              <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
+                <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
+              </div>
+            </div>
+            <div class="fw-light mt-4">
+              <div class="fw-bold">{{ $user->name }}</div>
+              <small>{{ $user->division }}</small>
+            </div>
+            <div class="footer d-flex justify-content-center align-items-center mt-3">
+              <a href="{{ route('admin.users.profile') }}"
+                class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
+                Profile
+              </a>
             </div>
           </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href="{{ route('admin.users.profile') }}"
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
         </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 mb-3">
-        <div class="p-4 text-white text-center"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
-          <div class="d-flex justify-content-center">
-            <div class="rounded-pill bg-dark overflow-hidden" style="width: 100px; height: 100px;">
-              <img src="{{ asset('assets/img/photo-profile.jpg') }}" alt="" width="100%" height="">
-            </div>
-          </div>
-          <div class="fw-light mt-4">
-            <div class="fw-bold">Irfan Yasin</div>
-            <small>Web Developer</small>
-          </div>
-          <div class="footer d-flex justify-content-center align-items-center mt-3">
-            <a href=""
-              class="border border-1 px-5 py-1 rounded-pill text-decoration-none text-center text-white fw-light">
-              Profile
-            </a>
-          </div>
-        </div>
-      </div>
+      @endforeach
     </div>
   </section>
 @endsection
