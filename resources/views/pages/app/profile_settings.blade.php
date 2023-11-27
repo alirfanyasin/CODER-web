4f1b2af413ae3cd8d77c71f3fd3c0d5a1b0844b4
alirfanyasin <risayunita21@gmail.com>
2023-11-28 00:28:53 +0700

    feat: authorization Update profile user and admin

--- resources/views/pages/app/profile_settings.blade.php
+++ resources/views/pages/app/profile_settings.blade.php
@@ -9,99 +9,104 @@
     </div>
   </section>
   <section>
-    <form action="{{ route('user.my-profile.settings.update', $data->id) }}" method="POST" enctype="multipart/form-data">
-      @csrf
-      @method('PUT')
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8">
-            <div class="text-white p-4"
-              style="background: rgba(255, 255, 255, 0.13); border-radius: 20px; backdrop-filter: blur(5px);">
-              <div class="input-group-custom mb-3">
-                <input type="text" name="name" class="form-control text-white fw-light"
-                  style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
+    @if (Auth::user()->hasRole('admin') || Auth::user()->hasPermissionTo('admin-division'))
+      <form action="{{ route('admin.my-profile.settings.update', $data->id) }}" method="POST" enctype="multipart/form-data">
+    @endif
+    @if (Auth::user()->hasRole('user'))
+      <form action="{{ route('user.my-profile.settings.update', $data->id) }}" method="POST" enctype="multipart/form-data">
+    @endif
+    @csrf
+    @method('PUT')
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="text-white p-4"
+            style="background: rgba(255, 255, 255, 0.13); border-radius: 20px; backdrop-filter: blur(5px);">
+            <div class="input-group-custom mb-3">
+              <input type="text" name="name" class="form-control text-white fw-light"
+                style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
                       backdrop-filter: blur(5px);"
-                  placeholder="Nama Lengkap" value="{{ $data->name }}">
-                @error('name')
-                  <small class="fw-light">{{ $message }}</small>
-                @enderror
-              </div>
-              <div class="input-group-custom mb-3">
-                <input type="number" name="nim" class="form-control text-white fw-light"
-                  style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
+                placeholder="Nama Lengkap" value="{{ $data->name }}">
+              @error('name')
+                <small class="fw-light">{{ $message }}</small>
+              @enderror
+            </div>
+            <div class="input-group-custom mb-3">
+              <input type="number" name="nim" class="form-control text-white fw-light"
+                style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
                       backdrop-filter: blur(5px);"
-                  placeholder="NIM" value="{{ $data->nim }}">
-                @error('nim')
-                  <small class="fw-light">{{ $message }}</small>
-                @enderror
-              </div>
-              <div class="input-group-custom mb-3">
-                <input type="text" name="field_of_study" class="form-control text-white fw-light"
-                  style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
+                placeholder="NIM" value="{{ $data->nim }}">
+              @error('nim')
+                <small class="fw-light">{{ $message }}</small>
+              @enderror
+            </div>
+            <div class="input-group-custom mb-3">
+              <input type="text" name="field_of_study" class="form-control text-white fw-light"
+                style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
                       backdrop-filter: blur(5px);"
-                  placeholder="Jurusan" value="{{ $data->field_of_study }}">
-                @error('field_of_study')
-                  <small class="fw-light">{{ $message }}</small>
-                @enderror
-              </div>
-              <div class="input-group-custom mb-3">
-                <input type="number" name="batch" class="form-control text-white fw-light"
-                  style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
+                placeholder="Jurusan" value="{{ $data->field_of_study }}">
+              @error('field_of_study')
+                <small class="fw-light">{{ $message }}</small>
+              @enderror
+            </div>
+            <div class="input-group-custom mb-3">
+              <input type="number" name="batch" class="form-control text-white fw-light"
+                style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
                       backdrop-filter: blur(5px);"
-                  placeholder="Tahun Angkatan" value="{{ $data->batch }}">
-                @error('batch')
-                  <small class="fw-light">{{ $message }}</small>
-                @enderror
-              </div>
-              <div class="input-group-custom mb-3">
-                <input type="text" name="email" class="form-control text-white fw-light"
-                  style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
+                placeholder="Tahun Angkatan" value="{{ $data->batch }}">
+              @error('batch')
+                <small class="fw-light">{{ $message }}</small>
+              @enderror
+            </div>
+            <div class="input-group-custom mb-3">
+              <input type="text" name="email" class="form-control text-white fw-light"
+                style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
                       backdrop-filter: blur(5px);"
-                  placeholder="Email" value="{{ $data->email }}" disabled>
-                @error('email')
-                  <small class="fw-light">{{ $message }}</small>
-                @enderror
-              </div>
-              <div class="input-group-custom mb-3">
-                <input type="number" name="phone_number" class="form-control text-white fw-light"
-                  style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
+                placeholder="Email" value="{{ $data->email }}" disabled>
+              @error('email')
+                <small class="fw-light">{{ $message }}</small>
+              @enderror
+            </div>
+            <div class="input-group-custom mb-3">
+              <input type="number" name="phone_number" class="form-control text-white fw-light"
+                style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
                       backdrop-filter: blur(5px);"
-                  placeholder="Nomor Telepon" value="{{ $data->phone_number }}">
-                @error('phone_number')
-                  <small class="fw-light">{{ $message }}</small>
-                @enderror
-              </div>
-              <div class="input-group-custom mb-3">
-                <input type="text" name="division_id" class="form-control text-white fw-light"
-                  style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
+                placeholder="Nomor Telepon" value="{{ $data->phone_number }}">
+              @error('phone_number')
+                <small class="fw-light">{{ $message }}</small>
+              @enderror
+            </div>
+            <div class="input-group-custom mb-3">
+              <input type="text" name="division_id" class="form-control text-white fw-light"
+                style="height: 50px; background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none; border-bottom: 2px solid white;
                       backdrop-filter: blur(5px);"
-                  placeholder="Division" value="{{ $data->division->name }}" disabled>
-                @error('division_id')
-                  <small class="fw-light">{{ $message }}</small>
-                @enderror
-              </div>
-              <div class="input-group-custom">
-                <button type="submit" class="text-white px-5 py-2"
-                  style="background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none;
+                placeholder="Division" value="{{ $data->division->name }}" disabled>
+              @error('division_id')
+                <small class="fw-light">{{ $message }}</small>
+              @enderror
+            </div>
+            <div class="input-group-custom">
+              <button type="submit" class="text-white px-5 py-2"
+                style="background: rgba(255, 255, 255, 0.02);  border-radius: 10px; border: none;
                   backdrop-filter: blur(5px);">Update</button>
-              </div>
             </div>
-          </div>
-          <div class="col-md-4">
-            <div class="p-3 text-white"
-              style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px); border-bottom: 2px solid white">
-              <img src="{{ asset('storage/avatar/' . ($data->avatar ?? 'photo-profile.jpg')) }}" id="result"
-                alt="" class="w-100" width="100%" style="border-radius: 10px;">
-            </div>
-            <input type="file" name="avatar" id="thumbnail" class="form-control mt-3 text-white"
-              style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);"
-              onchange="readFile(this)" value="{{ $data->avatar }}">
-            @error('avatar')
-              <small class="fw-light">{{ $message }}</small>
-            @enderror
           </div>
         </div>
+        <div class="col-md-4">
+          <div class="p-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px); border-bottom: 2px solid white">
+            <img src="{{ asset('storage/avatar/' . ($data->avatar ?? 'photo-profile.jpg')) }}" id="result"
+              alt="" class="w-100" width="100%" style="border-radius: 10px;">
+          </div>
+          <input type="file" name="avatar" id="thumbnail" class="form-control mt-3 text-white"
+            style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);"
+            onchange="readFile(this)" value="{{ $data->avatar }}">
+          @error('avatar')
+            <small class="fw-light">{{ $message }}</small>
+          @enderror
+        </div>
       </div>
+    </div>
     </form>
   </section>
 @endsection
