3967bbbe46673886f3a7096410ee8ce05bafde1d
alirfanyasin <risayunita21@gmail.com>
2023-11-28 20:53:37 +0700

    fix: landingpage UI using glassmorphism theme

--- resources/views/pages/app/user_profile.blade.php
+++ resources/views/pages/app/user_profile.blade.php
@@ -7,7 +7,12 @@
     <div class="container">
       <div class="row">
         <div class="col p-4"
-          style="background: rgba(255, 255, 255, 0.15); border-radius: 10px; backdrop-filter: blur(5px);">
+          style="background: rgba( 255, 255, 255, 0.1 );
+          box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+          backdrop-filter: blur( 8.5px );
+          -webkit-backdrop-filter: blur( 8.5px );
+          border-radius: 10px;
+          border: 1px solid rgba( 255, 255, 255, 0.18 );">
 
           <div class="row d-flex align-items-center">
             <div class="col-md-3">
